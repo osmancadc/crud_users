691048a700bbf29398947f063191af80e521c416
Osman Beltran <osmancadc@hotmail.com>
2021-02-03 11:12:00 -0500

    Patch and Delete request added

--- controller/databaseController.js
+++ controller/databaseController.js
@@ -3,20 +3,54 @@ const {
 } = require('mongodb');
 const uri = "mongodb+srv://osmancadc:4321@cluster0.ge4gy.mongodb.net/Users?retryWrites=true&w=majority";
 
-exports.getClient = ()=>{ return new MongoClient(uri)}
+exports.getClient = () => {
+    return new MongoClient(uri, {
+        useUnifiedTopology: true
+    })
+}
 
-exports.getUser = async (client,username,password,callback) => {
+exports.getUser = async (client, username, password, callback) => {
     await client.connect();
     result = await client.db("Users").collection("users")
-                        .findOne({ username: username,password:password });
-    await client.close();   
+        .findOne({
+            username: username,
+            password: password
+        });
+    await client.close();
     callback(result)
 }
 
-exports.getAllUsers = async (client,callback) =>{
+exports.getAllUsers = async (client, callback) => {
     await client.connect();
     result = await client.db("Users").collection("users")
-                        .find({ status:true}).toArray()
-    await client.close();   
+        .find({
+            status: true
+        }).toArray()
+    await client.close();
     callback(result)
+}
+
+exports.updateUser = async (client, idUser, data, callback) => {
+    await client.connect();
+
+    result = await client.db("Users").collection("users")
+        .updateOne({
+            id: parseInt(idUser)
+        }, {
+            $set: data
+        });
+    await client.close();
+    callback(result.matchedCount)
+}
+
+exports.deleteUser = async (client, idUser, callback) => {
+    await client.connect();
+
+    result = await client.db("Users").collection("users")
+        .deleteOne({
+            id: parseInt(idUser)
+        });
+
+    await client.close();
+    callback(result.deletedCount)
 }
--- controller/usersController.js
+++ controller/usersController.js
@@ -14,13 +14,47 @@ router.get("/", async (req, res) => {
 
         let client = databaseController.getClient()
 
-        databaseController.getAllUsers(client,async result => {
+        databaseController.getAllUsers(client, async result => {
             res.status(200).send(result)
         })
     } catch (err) {
         res.sendStatus(403)
     }
 
+});
+
+router.patch("/:id", async (req, res) => {
+    try {
+        let decoded = jwt.verify(req.headers.authorization.split(" ")[1], config.security.key);
+
+        let client = databaseController.getClient()
+
+        databaseController.updateUser(client, req.params.id, req.body, result => {
+            if (result > 0)
+                res.status(200).send("User updated successfully")
+            else
+                res.status(404).send("User not found")
+        })
+    } catch (err) {
+        res.sendStatus(403)
+    }
+});
+
+router.delete("/:id", async (req, res) => {
+    try {
+        let decoded = jwt.verify(req.headers.authorization.split(" ")[1], config.security.key);
+
+        let client = databaseController.getClient()
+
+        databaseController.deleteUser(client, req.params.id, result => {
+            if (result > 0)
+                res.status(200).send("User deleted successfully")
+            else
+                res.status(404).send("User not found")
+        })
+    } catch (err) {
+        res.sendStatus(403)
+    }
 });
 
 module.exports = router
